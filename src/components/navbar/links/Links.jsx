35ef79701e0a8603691dc7dd2d8ffae2ee571f2b
j-kyung99 <apfwnls14@naver.com>
2024-04-17 15:45:59 +0900

    #7 Next.js How to Give the Active Class to a Navbar Link

--- src/components/navbar/links/Links.jsx
+++ src/components/navbar/links/Links.jsx
@@ -1,4 +1,3 @@
-import Link from "next/link";
 import styles from "./links.module.css";
 import NavLink from "./navLink/navLink";
 
@@ -21,11 +20,24 @@ const Links = () => {
       path: "/blog",
     },
   ];
+
+  //TEMPORARY
+  const session = true;
+  const isAdmin = true;
+
   return (
     <div className={styles.links}>
       {links.map((link) => (
         <NavLink item={link} key={link.title} />
       ))}
+      {session ? (
+        <>
+          {isAdmin && <NavLink item={{ title: "Admin", path: "/admin" }} />}
+          <button className={styles.logout}>Logout</button>
+        </>
+      ) : (
+        <NavLink item={{ title: "Login", path: "/login" }} />
+      )}
     </div>
   );
 };
