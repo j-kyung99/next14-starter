1984d67b7b543fe8eefecc7e69e9202cccaf417a
j-kyung99 <apfwnls14@naver.com>
2024-04-24 15:59:42 +0900

    #14. Next.js Blog Page Design

---
+++ src/components/postCard/postCard.jsx
@@ -0,0 +1,35 @@
+import Image from "next/image";
+import styles from "./postCard.module.css";
+import Link from "next/link";
+
+const PostCard = () => {
+  return (
+    <div className={styles.container}>
+      <div className={styles.top}>
+        <div className={styles.imgContainer}>
+          <Image
+            src="https://images.pexels.com/photos/15079666/pexels-photo-15079666.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2"
+            alt=""
+            fill
+            className={styles.img}
+          />
+        </div>
+        <span className={styles.date}>01.01.2024</span>
+      </div>
+      <div className={styles.bottom}>
+        <h1 className={styles.title}>Title</h1>
+        <p className={styles.desc}>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Labore
+          aliquid ducimus nesciunt vero nisi iure maiores expedita dolorum?
+          Porro obcaecati eligendi aliquam excepturi reiciendis itaque fugit,
+          cupiditate in nemo perspiciatis.
+        </p>
+        <Link className={styles.link} href="/blog/post">
+          READ MORE
+        </Link>
+      </div>
+    </div>
+  );
+};
+
+export default PostCard;
